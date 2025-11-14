74c6b6035132c3a7fad13e69377af291a7d878d4
rossopoloco <rossopoloco@gmail.com>
2025-11-14 07:07:19 +0000

    Update oss.tf

--- oss.tf
+++ oss.tf
@@ -1,21 +1,21 @@
 resource "alicloud_oss_bucket" "this" {
   bucket = "${var.name_prefix}-${var.env}-${random_pet.suffix.id}"
-  acl    = "private"
   storage_class = "Standard"
 
   tags = local.tags
 }
 
-# 示例生命周期（把 30 天未访问的对象转IA，可按需调整）
-resource "alicloud_oss_bucket_lifecycle" "this" {
-  bucket = alicloud_oss_bucket.this.id
+## 示例生命周期（把 30 天未访问的对象转IA，可按需调整）
+#resource "alicloud_oss_bucket_lifecycle" "this" {
+#  bucket = alicloud_oss_bucket.this.id
 
-  rule {
-    id      = "to-ia-after-30d"
-    enabled = true
-    transition {
-      days          = 30
-      storage_class = "IA"
-    }
-  }
-}
+#  rule {
+#    id      = "to-ia-after-30d"
+#    enabled = true
+#    transition {
+#      days          = 30
+#      storage_class = "IA"
+#    }
+#  }
+#}
+
