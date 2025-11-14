1b4260993ba0e557b69a6815cbe519f94b3a142f
rossopoloco <rossopoloco@gmail.com>
2025-11-14 02:45:16 +0000

    Update versions.tf
    first update

--- versions.tf
+++ versions.tf
@@ -1,13 +1,19 @@
 terraform {
   required_version = ">= 1.6"
+
   required_providers {
     alicloud = {
       source  = "aliyun/alicloud"
-      version = "~> 1.210" # 需要确定，稳定大版本，必要时可放宽
+      version = ">= 1.233.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
     }
   }
-  cloud {
-    organization = "rosso"   # 需要确定，TFC 组织
-    workspaces { name = "alicloud-test" }  # 需要确定，对应你的工作空间名
-  }
+}
+
+provider "alicloud" {
+  # TFC 已通过环境变量注入：ALICLOUD_ACCESS_KEY/SECRET_KEY/REGION
+  region = var.region
 }
