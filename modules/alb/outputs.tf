1039faee69fa88d35ccb680b11db7f14465f62b3
rossopoloco <rossopoloco@gmail.com>
2025-11-27 06:00:22 +0000

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,8 +1,3 @@
-output "alb_dns_name" {
-  value       = alicloud_alb_load_balancer.this.dns_name
-  description = "ALB 公网域名"
-}
-
 output "alb_id" {
   value = alicloud_alb_load_balancer.this.id
 }
