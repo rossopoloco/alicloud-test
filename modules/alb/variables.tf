e77954f1c54a7795eb04659df9eac85362530592
rossopoloco <rossopoloco@gmail.com>
2025-11-27 03:08:47 +0000

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,26 +1,41 @@
-variable "name" { type = string }
-variable "env"         { type = string }
-variable "tags"      { type = map(string) }
+variable "name_prefix" {
+  type = string
+}
 
-variable "vpc_id"     { type = string }
-variable "zone_id"    { type = string }
-variable "vswitch_id" { type = string }
+variable "env" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "vswitch_id" {
+  type = string
+}
+
+variable "zone_id" {
+  type = string
+}
+
+variable "bandwidth_mbps" {
+  type    = number
+  default = 10
+}
 
 variable "listener_port" {
   type    = number
   default = 80
 }
 
-variable "backend_port" {
-  type    = number
-  default = 80
-}
-
-variable "backend_instance_ids" {
-  type = list(string)
+variable "server_instance_ids" {
+  type        = list(string)
+  description = "ECS 实例 ID 列表，将被挂到 ALB 后端组 80 端口"
+  default     = []
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "统一打到 ALB 相关资源上的标签"
+  default     = {}
 }
