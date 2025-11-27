e00968affff258b3594eb93cb1b40fef9b4eb958
rossopoloco <rossopoloco@gmail.com>
2025-11-27 05:56:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,20 +10,21 @@ data "alicloud_zones" "this" {
   available_resource_creation = "VSwitch"
 }
 
-module "alb" {
-  source = "./modules/alb"
-  count  = var.enable_alb ? 1 : 0
+#module "alb" {
+#  source = "./modules/alb"
+#  count  = var.enable_alb ? 1 : 0
+#
+#  name       = "${var.name}-${var.env}-alb"
+#  vpc_id     = alicloud_vpc.this.id
+#  vswitch_id = alicloud_vswitch.this.id
+#  zone_id    = data.alicloud_zones.this.zones[0].id
+#  env         = var.env
+#
+#  listener_port        = var.alb_listener_port
+#  backend_port         = 80
+#  backend_instance_ids = [for i in alicloud_instance.ecs : i.id]
+#
+#  tags = local.tags
+#}
 
-  name       = "${var.name}-${var.env}-alb"
-  vpc_id     = alicloud_vpc.this.id
-  vswitch_id = alicloud_vswitch.this.id
-  zone_id    = data.alicloud_zones.this.zones[0].id
-  env         = var.env
-
-  listener_port        = var.alb_listener_port
-  backend_port         = 80
-  backend_instance_ids = [for i in alicloud_instance.ecs : i.id]
-
-  tags = local.tags
-}
 
