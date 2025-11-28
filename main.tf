14943958cbbd71b3d99601940b8fe37afd2ba30a
rossopoloco <rossopoloco@gmail.com>
2025-11-28 02:02:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ data "alicloud_zones" "this" {
 # 只保留这一处 ALB 模块调用
 
 module "alb" {
+  count  = var.enable_alb ? 1 : 0
   source = "./modules/alb"
 
   # 从根模块把值传给子模块
@@ -31,4 +32,13 @@ module "alb" {
   backend_ecs_ids = [for i in alicloud_instance.ecs : i.id]
 
   tags = local.tags
+}
+
+
+module "waf" {
+  count  = var.enable_waf ? 1 : 0
+  source = "./modules/waf"
+  name_prefix = var.name_prefix
+  env         = var.env
+  # 其它 WAF 需要的入参（如果暂时没接入域名，可先留默认/占位，count=false 时不会创建）
 }
