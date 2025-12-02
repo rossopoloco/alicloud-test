8f1d8eabafab3376c0c51a2f6ff5731b1a9f217d
rossopoloco <rossopoloco@gmail.com>
2025-12-02 14:56:06 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
-output "eip" {
-  value = alicloud_eip_address.this.ip_address
-}
+#output "eip" {
+#  value = alicloud_eip_address.this.ip_address
+#}
 
-output "ecs_private_ips" {
-  value = [for i in alicloud_instance.ecs : i.private_ip]
-}
+#output "ecs_private_ips" {
+#  value = [for i in alicloud_instance.ecs : i.private_ip]
+#}
 
-output "oss_bucket" {
-  value = alicloud_oss_bucket.this.bucket
-}
+#output "oss_bucket" {
+#  value = alicloud_oss_bucket.this.bucket
+#}
 
-output "pg_endpoint" {
-  value = alicloud_db_instance.pg.connection_string
-}
+#output "pg_endpoint" {
+#  value = alicloud_db_instance.pg.connection_string
+#}
+
